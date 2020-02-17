4de553cb4021ac465f2d8fa42e6e696a9157ed49
Devon James <devjames98@gmail.com>
2020-02-17 16:45:54 -0500

    HW Done except diamond

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,13 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+  //empty string to store the reversed string
+  let reverse = "";
+  let lastIndex = someStr.length - 1;
+  for (let i = lastIndex; i >= 0; i--) {
+    reverse = reverse + someStr[i];
+  }
+  console.log(reverse);
 }
+
+reverseStr("hello");
--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,69 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
-  
+  //let print = "";
+  let i = 0;
+  switch (shape) {
+    case "Square": {
+      //let i = 0;
+      while (i < height) {
+        let print = "";
+        for (j = 0; j < height; j++) {
+          print = print + character;
+          //console.log(print);
+        }
+        console.log(print);
+        i++;
+      }
+      break;
+    }
+    case "Triangle": {
+      //let i = 0;
+      //while (i < height) {
+      let print = "";
+      for (j = 0; j < height; j++) {
+        print = print + character;
+        console.log(print);
+      }
+      //console.log();
+      i++;
+      //}
+      break;
+    }
+    //Diamond doesn't work
+    case "Diamond": {
+      let print = "";
+      let midpoint = (height - 1) / 2;
+      let start = midpoint;
+      let end = midpoint;
+
+      while (i < height) {
+        //left side
+        for (j = 0; j < start; j++) {
+          print = print + " ";
+        }
+        //print characters
+        for (let temp = start; temp <= end; temp++) {
+          print = print + character;
+        }
+        //right side
+        for (j = midpoint + 1; j < height; j++) {
+          print = print + " ";
+        }
+        i++;
+        start--;
+        end++;
+      }
+      break;
+    }
+    default:
+      console.log("Not a shape");
+
+      break;
+  }
 }
+
+printShape("Square", 3, "%");
+printShape("Triangle", 3, "$");
+//printShape("Diamond", 5, "*");
+//wrap switch code in brackets
